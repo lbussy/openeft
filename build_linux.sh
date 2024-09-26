24051f743f949536deb1498ffc13a4f36f8f29cb
Rob S <Robb.Stumpf@GMail.com>
2024-09-26 16:51:49 -0400

    Update build_linux.sh
    1. Added commands to remove old NBIS copies and clone a fresh version like in the Windows script.
    2. Added logic to check and update the NBIS binaries paths in ~/.bashrc for consistency.
    3. Added installation of libgl1-mesa-glx to ensure proper graphical library support, similar to the Windows build.
    4. Incorporated Django migrations (python3 manage.py migrate) to ensure the database is set up, matching the Windows build steps.

--- build_linux.sh
+++ build_linux.sh
@@ -1,30 +1,58 @@
+# This is untested!
 #!/usr/bin/bash
 
 mkdir build
 mkdir -p browser/linux
+
 echo "Updating Submodules"
 git submodule update
+
 echo "Setting Up NBIS"
+# Remove any old copies of NBIS
+sudo rm -rf ./nbis 
+git clone https://github.com/Robbbbbbbbb/nbis ./nbis
 cd nbis
+
 ./setup.sh ${PWD}/../build --64
+
 echo "Configuring NBIS"
 make config
+
 echo "Making NBIS"
 make it
+
 echo "Installing NBIS"
-make install
-echo "Adding NBIS binaries to path"
-echo 'export PATH="$PWD/build/bin:$PATH" > ~/.bashrc'
-source ~/.bashrc
+sudo make install LIBNBIS=no
+
+echo "Checking NBIS binaries paths"
+case :$PATH:
+  in *:$HOME/OpenEFT/nbis/nfseg/bin:*) ;; # do nothing, it's there
+     *) echo 'export PATH="$HOME/OpenEFT/nbis/nfseg/bin:$PATH"' >> ~/.bashrc #it's missing, add to PATH
+esac
+
+case :$PATH:
+  in *:$HOME/OpenEFT/nbis/nfiq/bin:*) ;; # do nothing, it's there
+     *) echo 'export PATH="$HOME/OpenEFT/nbis/nfiq/bin:$PATH"' >> ~/.bashrc #it's missing, add to PATH
+esac
+
+# Install the necessary tools
+echo "Installing LibOpenJP2-Tools"
+sudo apt-get install libopenjp2-tools -y
+sudo apt-get install libgl1-mesa-glx -y
+
+# Move to browser directory and set up chromium
 cd ../browser/linux
- wget "https://github.com/clickot/ungoogled-chromium-binaries/releases/download/111.0.5563.65-1/ungoogled-chromium_111.0.5563.65-1.1.AppImage"
+wget "https://github.com/clickot/ungoogled-chromium-binaries/releases/download/111.0.5563.65-1/ungoogled-chromium_111.0.5563.65-1.1.AppImage"
 mv ungoogled-chromium_111.0.5563.65-1.1.AppImage chrome.AppImage
+
 cd ../../
+
+# Install Python dependencies
 echo "Installing Pip Requirements"
 pip3 install -r requirements.txt
-echo "Installing LibOpenJP2-Tools"
-if [ ""$EUID"" != 0 ]; then
-    sudo apt-get install libopenjp2-tools
-fi
-echo "Starting OpenEFT"
-python3 openeft.py
+
+# Migrate Django (similar to Windows build)
+echo "Running Django Migrations"
+python3 manage.py migrate
+
+echo "OpenEFT build complete. Update your PATH by typing 'source ~/.bashrc' and run the app by typing 'python3 openeft.py'"

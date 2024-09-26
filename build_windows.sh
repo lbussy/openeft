fcf8e3b2f0dc927eb9e1cea2c8b25684f1fecab2
Rob S <Robb.Stumpf@GMail.com>
2024-09-26 12:13:00 -0400

    Update build_windows.sh
    Moved NBIS repo to my local known-working forked repo

--- build_windows.sh
+++ build_windows.sh
@@ -1,35 +1,41 @@
 #!/usr/bin/bash
-#wsl --install -d Ubuntu
 
 mkdir build
-mkdir browser/windows
-echo "Updating Submodules"
-git submodule update
-echo "Setting Up NBIS"
-cd nbis
-./setup.sh ${PWD}/../build --MSYS --64
 
+echo "Setting Up NBIS"
+sudo rm -rf ./nbis #Remove any old copies
+git clone https://github.com/Robbbbbbbbb/nbis ./nbis
+cd nbis
+./setup.sh ${PWD}/../build --64
 echo "Configuring NBIS"
 make config
 echo "Making NBIS"
 make it
 echo "Installing NBIS"
-make install
-echo "Adding NBIS binaries to path"
-echo 'export PATH="$PWD/build/bin:$PATH" > ~/.bashrc'
-source ~/.bashrc
-cd ../browser/windows
-wget "https://github.com/ungoogled-software/ungoogled-chromium-windows/releases/download/111.0.5563.65-1.1/ungoogled-chromium_111.0.5563.65-1.1_windows_x64.zip"
-unzip ungoogled-chromium_111.0.5563.65-1.1_windows_x64.zip
-mv ungoogled-chromium_111.0.5563.65-1.1_windows windows
-cd ../../
-echo "Installing Pip Requirements"
-pip3 install -r requirements.txt
-echo "Installing LibOpenJP2-Tools"
-if [ ""$EUID"" != 0 ]; then
-    wsl sudo apt-get install libopenjp2-tools
-fi
-echo "Starting OpenEFT"
-echo 'export PATH="$PWD/build/bin:$PATH" > ~/.bashrc
-python3 openeft.py
+sudo make install LIBNBIS=no
 
+echo "Checking NBIS binaries paths"
+case :$PATH:
+  in *:$HOME/OpenEFT/nbis/nfseg/bin:*) ;; # do nothing, it's there
+     *) echo 'export PATH="$HOME/OpenEFT/nbis/nfseg/bin:$PATH"' >> ~/.bashrc #it's missing, add to PATH
+esac
+
+case :$PATH:
+  in *:$HOME/OpenEFT/nbis/nfiq/bin:*) ;; # do nothing, it's there
+     *) echo 'export PATH="$HOME/OpenEFT/nbis/nfiq/bin:$PATH"' >> ~/.bashrc #it's missing, add to PATH
+esac
+
+#source ~/.bashrc doesn't work in .sh in Ubuntu since scripts run in their own session. Need to run post build
+
+echo "Installing LibOpenJP2-Tools"
+sudo apt-get install libopenjp2-tools -y
+sudo apt-get install libgl1-mesa-glx -y
+
+echo "Installing Pip Requirements"
+
+cd ~/OpenEFT
+pip3 install -r requirements.txt
+python3 manage.py migrate
+
+echo "OpenEFT build complete. Update your PATH by typing 'source ~/.bashrc' and run the app by typing 'python3 openeft.py'"
+cd ~/OpenEFT

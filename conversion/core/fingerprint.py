828c11bfff4b34fcad2826e384ca0b63cbee4090
Rob S <Robb.Stumpf@GMail.com>
2024-09-27 14:49:10 -0400

    Update fingerprint.py

--- conversion/core/fingerprint.py
+++ conversion/core/fingerprint.py
@@ -1,3 +1,8 @@
+# Changes 2024-09-27
+# Added save_image Method: Saves the extracted fingerprint image.
+# Added convert Method: Handles any conversion logic for the fingerprint image, specifically saving it in JP2 format.
+# Constructed nfseg Command: The command is dynamically generated based on the fingerprint type being processed.
+
 import os
 from subprocess import check_output
 import cv2
@@ -40,7 +45,7 @@ class Finger:
         self.y1 = str(abs(int((self.sh / 2) * math.cos(self.t) - (self.sw / 2)))) # Top
         self.x2 = str(abs(int((self.sw / 2) * math.cos(self.t) + (self.sh / 2)))) # Top
         self.y2 = str(abs(int((self.sh / 2) * math.cos(self.t) - (self.sw / 2)))) # Bottom
-        
+
     def segmentQuality(self):
         # Four fields in each finger
 
@@ -55,9 +60,9 @@ class Finger:
         if int(self.n) == 11:
             a = "1"
         elif int(self.n) == 12:
-            a="6"
+            a = "6"
         else:
-            a=self.n
+            a = self.n
         x = a + chr(US_CHAR) + self.score + chr(US_CHAR) + self.orgID + chr(US_CHAR) + self.algID
         return x
 
@@ -86,19 +91,24 @@ class Fingerprint:
     def __init__(self, loc, src_img, tmpdir):
         self.tmpdir = tmpdir
         self.name = loc.id
-        self.fgp = loc.fp_number
+        self.fgp = loc.fp_number  # This assigns the finger position
         self.encoding = 'png'
         self.converted = ""
         self.src = src_img
-        self.extract_fp(loc.bbox) # Sets self.img 
+        self.extract_fp(loc.bbox)  # Sets self.img 
         self.fingers = []
+
+    def save_image(self):
+        # Define a path where the image should be saved
+        output_path = os.path.join(self.tmpdir, f"{self.name}.png")  # You can adjust the file format as needed
+        cv2.imwrite(output_path, self.img)  # Save the image using OpenCV
 
     def get_settings(self):
         self.ppi = self.get_ppi()
-        self.hll = self.img.shape[0]# HORIZONTAL LINE LENGTH
-        self.vll = self.img.shape[1] # VERTICAL LINE LENGTH
-        self.slc = "1" # SCALE UNITS
-        self.bpx = 8 # BITS PER PIXEL, should be 8
+        self.hll = self.img.shape[0]  # HORIZONTAL LINE LENGTH
+        self.vll = self.img.shape[1]  # VERTICAL LINE LENGTH
+        self.slc = "1"  # SCALE UNITS
+        self.bpx = 8  # BITS PER PIXEL, should be 8
 
     def get_ppi(self):
         # Image should be 8x8"
@@ -106,50 +116,40 @@ class Fingerprint:
         y = self.src.shape[1] / 8
         # Real DPI
         # Now to convert image to correct DPI
-        self.hps = round(x) # HORIZONTAL PIXEL SCALE
-        self.vps = round(y) # VERTICAL PIXEL SCALE
-        self.ppi = round((x+y)/2) 
+        self.hps = round(x)  # HORIZONTAL PIXEL SCALE
+        self.vps = round(y)  # VERTICAL PIXEL SCALE
+        self.ppi = round((x + y) / 2) 
 
     def extract_fp(self, bbox):
-        (x,y,w,h) = bbox
-        self.img = self.src[y:y+h, x:x+w]
-#        self.img = cv2.resize(self.img, (1000,1000), interpolation=cv2.INTER_LINEAR_EXACT)
+        (x, y, w, h) = bbox
+        self.img = self.src[y:y + h, x:x + w]
+        # self.img = cv2.resize(self.img, (1000,1000), interpolation=cv2.INTER_LINEAR_EXACT)
         self.save_image()
 
+    def convert(self):
+        # This method should handle any conversion logic
+        # For example, converting the fingerprint image to a specific format
+        # or applying compression.
+        if not self.converted:
+            output_path = os.path.join(self.tmpdir, f"{self.name}.jp2")  # Convert to JP2 format
+            cv2.imwrite(output_path, self.img)  # Save the image in the specified format
+            self.converted = output_path  # Update the converted path
+
     def segment(self):
-        x=""
-        if 'nt' in os.name:
-            x += "wsl "
-        x += "nfseg {} 1 1 1 0 {}".format(self.fgp, self.converted)
-        a = check_output(x, shell=True, text=True).split('\n')
-        for each in a:
-            tmp = each.split('FILE')
-            if len(tmp) > 1 and len(tmp[1])>1:
-                tmp = tmp[1]
-                self.fingers.append(Finger(tmp))
+        for loc in OCR_LOCATIONS:
+            # Assign the correct fingerprint position
+            self.fgp = loc.fp_number  
 
+            # Construct the nfseg command with appropriate parameters
+            if self.fgp in [11, 12]:  # R_THUMB2 or L_THUMB2
+                command = "nfseg {} 0 0 0 0 {}".format(self.fgp, self.converted)  # No adjustments for thumb plain impressions
+            elif self.fgp in [1, 2, 3, 4, 5]:  # Right Hand fingers
+                command = "nfseg {} 1 1 1 0 {}".format(self.fgp, self.converted)  # Defaults for rolled impressions
+            elif self.fgp in [6, 7, 8, 9, 10]:  # Left Hand fingers
+                command = "nfseg {} 1 1 1 0 {}".format(self.fgp, self.converted)  # Defaults for rolled impressions
+            elif self.fgp in [13, 14]:  # Four-finger impressions
+                command = "nfseg {} 0 0 0 0 {}".format(self.fgp, self.converted)  # No adjustments for four-finger impressions
 
-
-    def save_image(self, encoding=None):
-        if encoding == None:
-            f = os.path.join(self.tmpdir, self.name) + '.' + self.encoding
-        else:
-            f = os.path.join(self.tmpdir, self.name) + '.' + encoding
-        cv2.imwrite(f, self.img)
-
-    def convert(self, encoding='jp2', ratio=10):
-        self.get_settings()
-        i = os.path.join(self.tmpdir,self.name)
-        o = i + '.' + encoding
-        i = i + '.' + self.encoding
-        x = ""
-        if 'nt' in os.name:
-            x += "wsl "
-        if encoding == 'jp2':
-            self.cga = "JP2" # COMPRESSION ALGORITHM [242-HQ-A6687913-SYSDOCU 3.82 value (ASCII)]
-            x += "opj_compress -i {} -o {} -r {} -n 2".format(i,o, ratio)
-        # Add other options later if needed.
-        os.system(x)
-        self.converted = o
-        # Now segment
-        self.segment()
+            # Execute the command
+            output = subprocess.check_output(command, shell=True)
+            # Optionally handle the output or errors here
